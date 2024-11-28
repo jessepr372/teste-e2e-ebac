f09bcb546bd83a136f17e01d9458e62db3f01457
jessepr372 <105854432+jessepr372@users.noreply.github.com>
2024-11-28 15:51:56 -0300

    Atualiza teste com melhorias no login e otimizações gerais

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,22 +1,47 @@
-/// <reference types="cypress" />
-
-context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
-      Quero acessar a Loja EBAC 
-      Para fazer um pedido de 4 produtos 
-      Fazendo a escolha dos produtos
-      Adicionando ao carrinho
-      Preenchendo todas opções no checkout
-      E validando minha compra ao final */
-
-  beforeEach(() => {
-      cy.visit('/')
+describe('Teste E2E na Loja EBAC', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
+  
+    // adicionar produto ao carrinho
+    Cypress.Commands.add('adicionarProduto', (categoriaSelector, produtoSelector, tamanho, cor) => {
+      cy.get(categoriaSelector).click();
+      cy.get(produtoSelector).click();
+      cy.get(`.button-variable-item-${tamanho}`).click();
+      cy.get(`.button-variable-item-${cor}`).click();
+      cy.get('.single_add_to_cart_button').click();
+      cy.get('.tbay-woocommerce-breadcrumb > :nth-child(1)').click();
+    });
+  
+    it('Deve adicionar 4 produtos ao carrinho e finalizar o pedido', () => {
+      const produtos = [
+        { categoria: '#primary-menu > .menu-item-629 > a', produto: '.post-3111 > .product-block', tamanho: 'XS', cor: 'Black' },
+        { categoria: '#primary-menu > .menu-item-629 > a', produto: '.post-3073 > .product-block', tamanho: '32', cor: 'Blue' },
+        { categoria: '#primary-menu > .menu-item-629 > a', produto: '.post-2622 > .product-block', tamanho: 'XS', cor: 'Blue' },
+        { categoria: '#primary-menu > .menu-item-629 > a', produto: '.post-3647 > .product-block', tamanho: 'XS', cor: 'Gray' },
+      ];
+  
+      // Adicionar os produtos ao carrinho
+      produtos.forEach(({ categoria, produto, tamanho, cor }) => {
+        cy.adicionarProduto(categoria, produto, tamanho, cor);
+      });
+  
+      // Acessar o carrinho de compras
+      cy.get('.dropdown-toggle > .zmdi').click();
+      cy.get('#topmenu > .menu-item-222 > a').click();
+  
+      // Preencher os dados de login
+      cy.get('.showlogin').click();
+      cy.get('#username').type('aluno_ebac@teste.com');
+      cy.get('#password').type('teste@teste.com');
+      cy.get('.woocommerce-button').click();
+  
+      // Finalizar o pedido
+      cy.get('#terms').click();
+      cy.get('#place_order').click();
+  
+      // Verificar mensagem de sucesso (com timeout de 10 segundos)
+      cy.get('.woocommerce-notice', { timeout: 10000 }).should('contain', 'Obrigado. Seu pedido foi recebido.');
+    });
   });
-
-  it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
-      //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
-
-
-})
+  
